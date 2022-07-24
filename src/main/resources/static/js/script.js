f9a4f6d1348a2b90a1630b43d28a4b5d78f4319b
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-07-24 19:38:34 +0300

    Stupid commit

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,12 +1,13 @@
-function changeImgOfTank(selectNode) {
+function changeImgOfTank(selectNode)
+{
     let tankId = selectNode.value;
 
     let callback = (response) => {
         let id = JSON.parse(response).category.id;
-        selectNode.parentNode.parentNode.getElementsByTagName("img")[0].src = "/images/tank-" + id + ".jpg";
+        selectNode.parentNode.parentNode.getElementsByTagName("img")[0].src = "/images/tank-"+id+".jpg";
     };
 
-    fetch('/tank/getCategoryId/' + tankId)
+    fetch('/tank/getCategoryId/'+tankId)
         .then()
         .then(callback);
 
