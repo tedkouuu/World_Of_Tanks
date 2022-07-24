8305590c001acf745564ecd35205498fae6ee167
tedkouuu <99691613+tedkouuu@users.noreply.github.com>
2022-07-24 19:52:43 +0300

    Script fix

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,14 +1,13 @@
-function changeImgOfTank(selectNode)
-{
+function changeImgOfTank(selectNode) {
     let tankId = selectNode.value;
 
     let callback = (response) => {
         let id = JSON.parse(response).category.id;
-        selectNode.parentNode.parentNode.getElementsByTagName("img")[0].src = "/images/tank-"+id+".jpg";
+        selectNode.parentNode.parentNode.getElementsByTagName("img")[0].src = "/images/tank-" + id + ".jpg";
     };
 
-    fetch('/tank/getCategoryId/'+tankId)
-        .then()
+    fetch('/tank/getCategoryId/' + tankId)
+        .then(response => response.text())
         .then(callback);
 
 
